63710ef3d1a3e24bf357f7e23cdbebfb2f449275
zhuziyu <zhuziyu0727@foxmail.com>
2024-04-28 16:28:09 +0800

    for uwb

--- system/utils/config_utils.py
+++ system/utils/config_utils.py
@@ -2,7 +2,7 @@ import argparse
 def argparser():
     parser = argparse.ArgumentParser()
     parser.add_argument('--mode', type=str, default='debug', help='Choose mode such as debug, single_wandb')
-    parser.add_argument('--project', type=str, default='FedCHAR', help='project name for wandb')
+    parser.add_argument('--project', type=str, default='FedCHAR_DC', help='project name for wandb')
     parser.add_argument('--device', type=str, default='cuda')
     parser.add_argument('--device_id', type=str, default='0')
     parser.add_argument('--algorithm', type=str, default='FedCHAR')
